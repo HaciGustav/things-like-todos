dde9198ec4a3ae7ddebafc7dd7709691da78a17b
MehmetFK <mehmetfkuruldak@gmail.com>
2022-10-21 13:54:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [thing, setThing] = useState(['Thing 1', 'Thing 2']);
+
+    const handleAdd = () => {
+        const newThing = `thing ${thing.length + 1}`;
+
+        thingsArray.push(newThing);
+        setThing((prevState) => [...prevState, newThing]);
+        console.log(thingsArray);
+    };
+    useEffect(() => {}, [thing]);
+
+    const thingsArray = [];
+
+    return (
+        <div className="App">
+            <Button
+                className="mt-5"
+                variant="outline-success"
+                size="lg"
+                onClick={() => handleAdd()}>
+                Add Thing
+            </Button>
+            <div className="d-flex flex-column align-items-center">
+                {thing.map((item) => (
+                    <p
+                        className="mt-3 p-3 border border-success rounded w-50"
+                        key={item}>
+                        {item}
+                    </p>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
